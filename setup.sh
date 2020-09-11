847030fef3efb4ac4f8c5323f12e9f855312fd38
Arjun Sunil <arjun921@gmail.com>
2020-09-11 22:37:00 +0530

    added autoenv

--- setup.sh
+++ setup.sh
@@ -29,3 +29,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip && sudo ./aws/install
 rm -rf awscliv2.zip aws
+
+# auto env
+# cd directory_with_.env_file
+# executes .env in shell
+pip install autoenv
